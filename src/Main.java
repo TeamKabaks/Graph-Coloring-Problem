5eb7793f3d2447240ed1d7940fdafd8f7deea770
Norman Enrico C. Eulin <eulinnormanenricocatalan@gmail.com>
2025-03-04 22:16:48 +0800

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ButtonOperations bO = new ButtonOperations();
-        MainGUI gui = new MainGUI(bO);
-        gui.createGUI();
-    }   
-}
